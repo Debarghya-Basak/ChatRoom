494e9dba4ec918109f19632022169eac2269c887
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2023-07-19 18:35:44 +0530

    Added Basic Splash Screen

--- app/src/main/java/com/dbtapps/chatroom/MainActivity.java
+++ app/src/main/java/com/dbtapps/chatroom/MainActivity.java
@@ -1,14 +1,26 @@
 package com.dbtapps.chatroom;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialToolbar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = findViewById(R.id.action_bar);
+        setSupportActionBar(actionBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
     }
 }
